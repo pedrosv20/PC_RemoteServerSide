25472b22ada6b65714aba1d5c1d1cd31b87b6f94
Pedro <pedro1silva1vargas@gmail.com>
2020-01-03 15:31:47 -0300

    commented testclient

--- PC_RemoteServerSide/src/pc_remoteserverside/TestClient.java
+++ PC_RemoteServerSide/src/pc_remoteserverside/TestClient.java
@@ -1,73 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pc_remoteserverside;
-
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.concurrent.Semaphore;
-
-/**
- *
- * @author Pedro Vargas
- */
-public class TestClient {
-      
-    /**
-     * @param args
-     */
-    
-    
-    public static void main(String[] args) throws InterruptedException  {
-        // TODO Auto-generated method stub
-        // abre conexÃ£o com o servido
-        System.out.println("cd \\");
-       
-        try {
-            
-           Socket s = new Socket("192.168.0.15", 2000);
-           DataOutputStream output = new DataOutputStream(s.getOutputStream());
-           output.write("aumentar volume".getBytes());
-           
-           
-          /*
-            DataInputStream ois = new DataInputStream(s.getInputStream());
-            ArrayList<String> users = new ArrayList();
-            String a =  "";
-            Thread.currentThread().sleep(1000); 
-            
-            while (ois.available() != 0) {
-               a = ois.readUTF();
-               users.add(a);
-            }
-
-            System.out.println(users);
-            
-            System.out.println("Qual seu nome");
-            Scanner meu = new Scanner(System.in);
-            String user = meu.nextLine();
-            Semaphore mutexEnviar = new Semaphore(1);
-            Semaphore mutexReceber = new Semaphore(0);
-            ThreadEnviar envia = new ThreadEnviar(user, s, users, mutexEnviar, mutexReceber);
-
-            ThreadReceber recebe = new ThreadReceber(user, s, mutexEnviar, mutexReceber);
-            envia.start();
-            recebe.start();
-          */
-
-            
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package pc_remoteserverside;
+//
+//import java.io.DataInputStream;
+//import java.io.DataOutputStream;
+//import java.io.IOException;
+//import java.net.Socket;
+//import java.net.UnknownHostException;
+//import java.util.ArrayList;
+//import java.util.Scanner;
+//import java.util.concurrent.Semaphore;
+//
+///**
+// *
+// * @author Pedro Vargas
+// */
+//public class TestClient {
+//      
+//    /**
+//     * @param args
+//     */
+//    
+//    
+//    public static void main(String[] args) throws InterruptedException  {
+//        // TODO Auto-generated method stub
+//        // abre conexÃ£o com o servido
+//        System.out.println("cd \\");
+//       
+//        try {
+//            
+//           Socket s = new Socket("192.168.0.15", 2000);
+//           DataOutputStream output = new DataOutputStream(s.getOutputStream());
+//           output.write("aumentar volume".getBytes());
+//           
+//           
+//          /*
+//            DataInputStream ois = new DataInputStream(s.getInputStream());
+//            ArrayList<String> users = new ArrayList();
+//            String a =  "";
+//            Thread.currentThread().sleep(1000); 
+//            
+//            while (ois.available() != 0) {
+//               a = ois.readUTF();
+//               users.add(a);
+//            }
+//
+//            System.out.println(users);
+//            
+//            System.out.println("Qual seu nome");
+//            Scanner meu = new Scanner(System.in);
+//            String user = meu.nextLine();
+//            Semaphore mutexEnviar = new Semaphore(1);
+//            Semaphore mutexReceber = new Semaphore(0);
+//            ThreadEnviar envia = new ThreadEnviar(user, s, users, mutexEnviar, mutexReceber);
+//
+//            ThreadReceber recebe = new ThreadReceber(user, s, mutexEnviar, mutexReceber);
+//            envia.start();
+//            recebe.start();
+//          */
+//
+//            
+//        } catch (UnknownHostException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//}
